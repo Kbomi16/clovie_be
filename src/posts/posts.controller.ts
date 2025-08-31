829de7e23cd0c617dfb3bdd3c121ef942044ed29
Kbomi16 <kimbomi2172@naver.com>
2025-08-31 17:27:42 +0900

    ✨ 게시물 수정, 삭제 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { User } from 'src/common/decorators/user.decorator';
@@ -9,21 +17,31 @@ import { Public } from 'src/common/decorators/public.decorator';
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Public()
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Public()
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
+  }
+
   // @User() 데코레이터 -> 인증 토큰에서 추출한, 현재 로그인된 사용자의 전체 정보 객체
   @Post()
   create(@Body() createPostDto: CreatePostDto, @User() user: UserEntity) {
     return this.postsService.create(createPostDto, user);
   }
 
-  @Public()
-  @Get()
-  async findAll() {
-    return await this.postsService.findAll();
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: CreatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
   }
 
-  @Public()
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.postsService.findOne(+id);
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.delete(+id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -82,5 +82,59 @@ export class PostsService {
     });
 
     return new SuccessResponse(`id가 ${id}인 게시글을 조회합니다.`, post);
+  }
+
+  // ! 특정 게시물 수정
+  async update(id: number, updatePostDto: CreatePostDto) {
+    // 1. 게시글 조회
+    const post = await this.postsRepository.findOne({ where: { id } });
+    if (!post)
+      throw new HttpException(
+        '게시글을 찾을 수 없습니다.',
+        HttpStatus.NOT_FOUND,
+      );
+
+    // 2. 수정
+    Object.assign(post, updatePostDto);
+    await this.postsRepository.save(post);
+
+    // 3. 조회할 때처럼 select + relations 포함
+    const updatedPost = await this.postsRepository.findOne({
+      where: { id },
+      select: {
+        id: true,
+        title: true,
+        tags: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        author: {
+          id: true,
+          nickname: true,
+          email: true,
+        },
+      },
+      relations: { author: true },
+    });
+
+    return new SuccessResponse(
+      '게시글이 성공적으로 수정되었습니다.',
+      updatedPost,
+    );
+  }
+
+  // ! 특정 게시물 삭제
+  async delete(id: number) {
+    // 1. 게시글 조회
+    const post = await this.postsRepository.findOne({ where: { id } });
+    if (!post)
+      throw new HttpException(
+        '게시글을 찾을 수 없습니다.',
+        HttpStatus.NOT_FOUND,
+      );
+
+    // 2. 삭제
+    await this.postsRepository.delete(id);
+    return new SuccessResponse('게시글이 성공적으로 삭제되었습니다.', null);
   }
 }
